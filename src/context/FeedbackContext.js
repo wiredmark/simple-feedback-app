1602f599e4006c32908ec40aa56e54872d4c939a
Wiredmark <development@wiredmark.co.uk>
2022-08-22 14:55:38 +0100

    Added loading spinner

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,6 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
 import { createContext, useState } from 'react'
-import FeedbackData from '../data/FeedbackData'
 import { useEffect } from 'react'
 
 const FeedbackContext = createContext()
