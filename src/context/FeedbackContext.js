a0c2de730119b2b167adf2b4b196318375bafd5b
Wiredmark <development@wiredmark.co.uk>
2022-08-22 14:36:48 +0100

    json-server implementation

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,15 +1,32 @@
 import { v4 as uuidv4 } from 'uuid'
 import { createContext, useState } from 'react'
 import FeedbackData from '../data/FeedbackData'
+import { useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?_sort=id&_order=desc`
+    )
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   // Add feedback
   const addFeedback = (newFeedback) => {
@@ -44,6 +61,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
